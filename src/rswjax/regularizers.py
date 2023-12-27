49b8b39d09d39a9ac11b3e79eb213de2163c5009
andytimm <timmandrew1@gmail.com>
2023-12-27 12:45:48 -0500

    entropy regularizer

--- src/rswjax/regularizers.py
+++ src/rswjax/regularizers.py
@@ -8,3 +8,17 @@ class ZeroRegularizer():
 
     def prox(self, w, lam):
         return w
+
+class EntropyRegularizer():
+
+    def __init__(self, limit=None):
+        if limit is not None and limit <= 1:
+            raise ValueError(f"limit is {limit:.3f}. It must be > 1.")
+        self.limit = limit
+
+    def prox(self, w, lam):
+        what = lam * jnp.real(tfp.lambertw(jnp.exp(w / lam - 1) / lam))
+        if self.limit is not None:
+            what = jnp.clip(what, 1 / (self.limit * w.size),
+                           self.limit / w.size)
+        return what
--- tests/test_regularizers.py
+++ tests/test_regularizers.py
@@ -11,3 +11,10 @@ lam = .5
 def test_zero_regularizer():
     zero_reg = rswjax.ZeroRegularizer()
     np.testing.assert_allclose(zero_reg.prox(w, .5), w)
+
+def test_entropy_regularizer():
+    entropy_reg = rswjax.EntropyRegularizer()
+    what = cp.Variable(10)
+    cp.Problem(cp.Minimize(-cp.sum(cp.entr(what)) + 1 /
+                           (2 * lam) * cp.sum_squares(what - w))).solve(solver=cp.ECOS)
+    np.testing.assert_allclose(what.value, entropy_reg.prox(w, lam), atol=1e-4)
