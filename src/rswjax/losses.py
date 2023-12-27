2c2db28e89523cfcabaa1e97290aa2875bc53b7e
andytimm <timmandrew1@gmail.com>
2023-12-27 11:51:56 -0500

    ineq loss and tests

--- src/rswjax/losses.py
+++ src/rswjax/losses.py
@@ -11,3 +11,17 @@ class EqualityLoss():
 
     def prox(self, f, lam):
         return self.fdes
+    
+class InequalityLoss():
+
+    def __init__(self, fdes, lower, upper):
+        if isinstance(fdes, Number):
+            fdes = jnp.array([fdes])
+        self.fdes = fdes
+        self.m = fdes.size
+        self.lower = lower
+        self.upper = upper
+        assert (self.lower <= self.upper).all()
+
+    def prox(self, f, lam):
+        return jnp.clip(f, self.fdes + self.lower, self.fdes + self.upper)
--- tests/test_losses.py
+++ tests/test_losses.py
@@ -6,6 +6,8 @@ m = 10
 f = np.random.randn(m)
 fdes = np.random.randn(m)
 lam = 1
+lower = np.array([-.3])
+upper = np.array([.3])
 
 def test_equality_loss():
     equality = rswjax.EqualityLoss(fdes)
@@ -13,3 +15,10 @@ def test_equality_loss():
     cp.Problem(cp.Minimize(1 / lam * cp.sum_squares(fhat - f)),
                 [fhat == fdes]).solve()
     np.testing.assert_allclose(fhat.value, equality.prox(f, lam))
+
+def test_inequality_loss():
+    inequality = rswjax.InequalityLoss(fdes, lower, upper)
+    fhat = cp.Variable(m)
+    cp.Problem(cp.Minimize(1 / lam * cp.sum_squares(fhat - f)),
+               [lower <= fhat - fdes, fhat - fdes <= upper]).solve()
+    np.testing.assert_allclose(fhat.value, inequality.prox(f, lam))
